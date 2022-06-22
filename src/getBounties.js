6303a021a15f6cf0d45c8eaba9603f194c0ecf62
FlacoJones <andrew@openq.dev>
2022-06-22 18:42:09 -0500

    more updates

--- src/getBounties.js
+++ src/getBounties.js
@@ -1,23 +1,26 @@
+const GET_ALL_BOUNTIES = require('./graphql/getAllBounties');
+const axios = require('axios');
+
 const getBounties = async (sortOrder, startAt, quantity) => {
 	const promise = new Promise(async (resolve, reject) => {
-			try {
-				result = await axios
-					.post(
-						`${process.env.OPENQ_SUBGRAPH_HTTP_URL}/graphql`,
-						{
-							query: GET_ALL_BOUNTIES,
-							variables: { skip: startAt, sortOrder, quantity }
+		try {
+			result = await axios
+				.post(
+					`${process.env.OPENQ_SUBGRAPH_HTTP_URL}/graphql`,
+					{
+						query: GET_ALL_BOUNTIES,
+						variables: { skip: startAt, sortOrder, quantity }
+					},
+					{
+						headers: {
+							'Authorization': process.env.OPENQ_API_SECRET,
 						},
-						{
-							headers: {
-								'Authorization': process.env.OPENQ_API_SECRET,
-							},
-						}
-					);
+					}
+				);
 			resolve(result.data.bounties);
 		} catch (e) {
-		console.error(e)
-			resolve([])
+			console.error(e);
+			resolve([]);
 		}
 		resolve([]);
 	});
