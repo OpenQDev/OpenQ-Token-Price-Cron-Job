c8f6d057881cc923e7b1e1d2b3b05014fcd8c563
FlacoJones <andrew@openq.dev>
2022-06-23 13:05:52 -0500

    adds dotenv

--- src/indexer.js
+++ src/indexer.js
@@ -1,15 +1,18 @@
 const updateTopTenPrices = require('./updateTopTenPrices');
+const updateTvls = require('./updateTvls');
+const fetchTvls = require('./fetchTvls');
 
 require('dotenv').config();
 
 const indexer = async () => {
 	try {
-		const result = await updateTopTenPrices();
-		// await updateTvls(await fetchTvls());
+		// const result = await updateTopTenPrices();
+		const result = await updateTvls(await fetchTvls(process.env.DEPLOY_ENV));
+		console.log(result);
 	}
 	catch (error) {
 		// GraphQL errors at error.response.data.errors
-		console.error('An error occured: ', error.response.data.errors);
+		console.error('An error occured: ', error);
 	}
 };
 
