0c65f59e845fcc68936ae9aef99f5404ae16ac58
FlacoJones <andrew@openq.dev>
2022-06-24 17:00:05 -0500

    removes /graphql

--- src/getBounties.js
+++ src/getBounties.js
@@ -13,7 +13,7 @@ const getBounties = async (sortOrder, startAt, quantity) => {
 		try {
 			result = await axios
 				.post(
-					`${process.env.OPENQ_SUBGRAPH_HTTP_URL}/graphql`,
+					`${process.env.OPENQ_SUBGRAPH_HTTP_URL}`,
 					{
 						query: GET_ALL_BOUNTIES,
 						variables: { skip: startAt, sortOrder, quantity }
