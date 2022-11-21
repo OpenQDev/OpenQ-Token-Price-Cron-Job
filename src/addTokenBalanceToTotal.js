7e508905b523ab568c555e6cef496f7e85834900
Christopher Stevers <christopher.stevers@protonmail.com>
2022-11-21 16:23:26 -0500

    handles unknown tokens

--- src/addTokenBalanceToTotal.js
+++ src/addTokenBalanceToTotal.js
@@ -4,11 +4,18 @@ const { getAddress } = require('@ethersproject/address');
 const addTokenBalanceToTotal = (runningTotal, tokenBalance, tokenMetadata, priceData) => {
 	const checksummedTokenAddress = getAddress(tokenBalance.tokenAddress);
 	const lowerCaseTokenAddress = tokenBalance.tokenAddress.toLowerCase();
+	try{
 	const currentMetadata = tokenMetadata[checksummedTokenAddress] || polygonMetadata[lowerCaseTokenAddress];
 	const multiplier = tokenBalance.volume / 10 ** currentMetadata.decimals;
 	const price = priceData[currentMetadata.address.toLowerCase()];
 	const newTotal = price.usd * multiplier + parseFloat(runningTotal);
 	return newTotal;
+	}
+	catch(error){
+		console.log(error);
+		return 0;
+
+	}
 };
 
 module.exports = addTokenBalanceToTotal; 
