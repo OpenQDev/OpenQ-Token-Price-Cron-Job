6a79e5696f5282cf242459748d92a6e4b60f768b
FlacoJones <andrew@openq.dev>
2023-03-05 16:25:11 -0700

    more coingecko pro updates

--- src/fetchCoingeckoPrices.js
+++ src/fetchCoingeckoPrices.js
@@ -3,7 +3,12 @@ const axios = require('axios');
 const fetchCoingeckoPrices = async (pricingMetadata) => {
 	const network = 'polygon-pos';
 	const commaDelimitedTokenAddresses = pricingMetadata.map((metadata) => metadata.address).join(',');
-	const url = `https://api.coingecko.com/api/v3/simple/token_price/${network}?contract_addresses=${commaDelimitedTokenAddresses}&vs_currencies=usd`;
+	
+	let url = `https://api.coingecko.com/api/v3/simple/token_price/${network}?contract_addresses=${commaDelimitedTokenAddresses}&vs_currencies=usd`;
+  if (process.env.COINGECK_API_KEY) {
+    url = `${url}&x_cg_pro_api_key=${process.env.COINGECK_API_KEY}`;
+  }
+
 	const { data } = await axios.get(url);
 	return data;
 };
