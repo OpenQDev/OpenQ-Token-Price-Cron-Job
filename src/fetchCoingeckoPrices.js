21b88922df5ed2cbe95d6db05ddd1971d7c28275
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2023-04-19 09:27:19 +0000

    added non api authenticated switch for local dev

--- src/fetchCoingeckoPrices.js
+++ src/fetchCoingeckoPrices.js
@@ -4,9 +4,9 @@ const fetchCoingeckoPrices = async (pricingMetadata) => {
 	const network = 'polygon-pos';
 	const commaDelimitedTokenAddresses = pricingMetadata.map((metadata) => metadata.address).join(',');
 	
-	let url = `https://pro-api.coingecko.com/api/v3/simple/token_price/${network}?contract_addresses=${commaDelimitedTokenAddresses}&vs_currencies=usd`;
+	let url = `https://api.coingecko.com/api/v3/simple/token_price/${network}?contract_addresses=${commaDelimitedTokenAddresses}&vs_currencies=usd`;
   if (process.env.COINGECK_API_KEY) {
-    url = `${url}&x_cg_pro_api_key=${process.env.COINGECK_API_KEY}`;
+    url = `https://pro-api.coingecko.com/api/v3/simple/token_price/${network}?contract_addresses=${commaDelimitedTokenAddresses}&vs_currencies=usd&x_cg_pro_api_key=${process.env.COINGECK_API_KEY}`;
   }
 
 	const { data } = await axios.get(url);
